90ada1cab041ae4d158b2ebfc1d9849f7026f95c
Nathn <nathan.tranchant@epitech.eu>
2023-02-07 16:53:26 +0100

    [ADD] place in leaderboard in profile

--- commands/profil.js
+++ commands/profil.js
@@ -8,19 +8,31 @@ module.exports = {
     async execute(interaction) {
         const Kebab = mongoose.model('Kebab');
         const kebabs = await Kebab.find({ user: interaction.user.id }).sort({ datetime: 1 });
+        const allkebabs = await Kebab.aggregate([
+            { $group: { _id: '$user', count: { $sum: 1 } } },
+            { $sort: { count: -1 } }
+        ]);
+        const placeinleaderboard = allkebabs.findIndex(kebab => kebab._id == interaction.user.id) + 1;
+        let reply = '';
         switch (kebabs.length) {
             case 0:
-                await interaction.reply('Vous n\'avez jamais mangé de kebab.');
-                return;
+                reply += 'Vous n\'avez jamais mangé de kebab.';
+                break;
             case 1:
-                await interaction.reply(`Vous avez mangé un seul kebab.
-Il a été mangé le ${kebabs[0].datetime.toLocaleDateString('fr-FR', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}.`);
-                return;
+                reply += `Vous avez mangé un seul kebab.
+Il a été mangé le ${kebabs[0].datetime.toLocaleDateString('fr-FR', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}.`;
+                break;
             default:
-                await interaction.reply(`Vous avez mangé ${kebabs.length} kebab${kebabs.length > 1 ? 's' : ''}.
+                reply += `Vous avez mangé ${kebabs.length} kebab${kebabs.length > 1 ? 's' : ''}.
 Votre premier kebab a été mangé le ${kebabs[0].datetime.toLocaleDateString('fr-FR', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}.
-Votre dernier kebab a été mangé le ${kebabs[kebabs.length - 1].datetime.toLocaleDateString('fr-FR', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}.`);
-                return;
+Votre dernier kebab a été mangé le ${kebabs[kebabs.length - 1].datetime.toLocaleDateString('fr-FR', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}.`;
+            break;
         }
+        if (placeinleaderboard == 1) {
+            reply += '\nVous êtes premier du classement des gros mangeurs de kebabs !';
+        } else {
+            reply += `\nVous êtes ${placeinleaderboard}ème du classement des gros mangeurs de kebabs.`;
+        }
+        await interaction.reply(reply);
     }
 };
