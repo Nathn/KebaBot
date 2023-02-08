f68e5578eef00e326ded40ef1cbf3c2b7af29a3a
Nathn <nathan.tranchant@epitech.eu>
2023-02-08 12:01:37 +0100

    [UPDATE] /rmkebab to /vomi

---
+++ commands/vomi.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('discord.js');
+const mongoose = require('mongoose');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+        .setName('vomi')
+        .setDescription('Vommissez un kebab.'),
+    async execute(interaction) {
+        const Kebab = mongoose.model('Kebab');
+        const kebab = await Kebab.findOne({ user: interaction.user.id }).sort({ date: -1 });
+        if (!kebab) {
+            await interaction.reply('Vous n\'avez jamais mangé de kebab.');
+            return;
+        }
+        await kebab.delete();
+        await interaction.reply('Vous avez vomi un kebab.');
+    }
+};
