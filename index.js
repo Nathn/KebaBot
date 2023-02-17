3bd09a6907f44e84f56d6f9872bd7c83e5270b03
Nathn <nathan.tranchant@epitech.eu>
2023-02-17 13:12:36 +0100

    [UPDATE] send error message before exiting

--- index.js
+++ index.js
@@ -74,8 +74,8 @@ client.on(Events.InteractionCreate, async interaction => {
 	try {
 		await command.execute(interaction);
 	} catch (error) {
-		console.error(error);
 		await interaction.reply({ content: 'Une erreur est survenue lors de l\'exécution de la commande. Zola a dit j\'accuse, moi je m\'excuse.', ephemeral: true });
+		console.error(error);
 	}
 });
 
