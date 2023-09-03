2358a2095314b06e666b0a7df53896e44be9da39
Nathn <nathan@tranchant.fr>
2023-09-03 13:34:38 +0200

    Add /palmares command

---
+++ commands/palmares.js
@@ -0,0 +1,166 @@
+const { SlashCommandBuilder } = require("discord.js");
+const mongoose = require("mongoose");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("palmares")
+        .setDescription("Affiche le palmarès d'un mangeur de kebabs.")
+        .addStringOption((option) =>
+            option
+                .setName("user")
+                .setDescription(
+                    "L'utilisateur dont vous voulez voir le palmarès."
+                )
+                .setRequired(false)
+        ),
+    async execute(interaction) {
+        const Kebab = mongoose.model("Kebab");
+        let kebabs,
+            user,
+            reply = "",
+            customuser = false;
+        if (interaction.options.getString("user"))
+            user = interaction.options.getString("user").replace(/[<@!>]/g, "");
+        if (
+            interaction.options.getString("user") &&
+            user != interaction.user.id
+        ) {
+            customuser = true;
+            reply += `**Palmarès de ${interaction.options.getString(
+                "user"
+            )} :**\n\n`;
+        } else {
+            reply += `**Votre palmarès toutes saisons confondues :**\n\n`;
+        }
+        // Get current year
+        const now = new Date();
+        const currentYear = now.getFullYear();
+        let start, end, allkebabs, placeinleaderboard;
+        // for each season between 2022 and current year
+        for (let season = 2022; season <= currentYear; season++) {
+            start = new Date(season, 8, 1); // September 1st of variable season
+            end = new Date(season + 1, 7, 31); // August 31st of season+1
+            allkebabs = await Kebab.aggregate([
+                { $match: { datetime: { $gte: start, $lte: end } } },
+                { $group: { _id: "$user", count: { $sum: 1 } } },
+                { $sort: { count: -1 } },
+            ]);
+            if (customuser) {
+                placeinleaderboard =
+                    allkebabs.findIndex((kebab) => kebab._id == user) + 1;
+            } else {
+                placeinleaderboard =
+                    allkebabs.findIndex(
+                        (kebab) => kebab._id == interaction.user.id
+                    ) + 1;
+            }
+            switch (placeinleaderboard) {
+                case 1:
+                    reply += `:trophy: Champion de la saison ${season}-${
+                        season + 1
+                    } (x${allkebabs[0].count})\n`;
+                    break;
+                case 2:
+                    reply += `:second_place: Vice-champion de la saison ${season}-${
+                        season + 1
+                    } (x${allkebabs[1].count})\n`;
+                    break;
+                case 3:
+                    reply += `:third_place: Troisième de la saison ${season}-${
+                        season + 1
+                    } (x${allkebabs[2].count})\n`;
+                    break;
+                default:
+                    break;
+            }
+            // create an empty object to store the ratios for each user
+            const ratios = {};
+
+            // loop through each user in the allkebabs array
+            for (const kebab of allkebabs) {
+                // get the user's name and kebab count
+                const { _id: name, count: kebabCount } = kebab;
+                // sort the user's kebabs by date
+                const userKebabs = await Kebab.find({
+                    user: name,
+                    datetime: { $gte: start, $lte: end },
+                }).sort({ datetime: 1 });
+                if (userKebabs.length < 5) continue; // skip users with less than 5 kebabs
+                // calculate the number of days between the first and last kebab
+                const firstKebab = userKebabs[0]?.datetime;
+                const lastKebab = userKebabs[userKebabs.length - 1]?.datetime;
+                const days =
+                    firstKebab && lastKebab
+                        ? Math.round(
+                              (lastKebab - firstKebab) / (1000 * 60 * 60 * 24)
+                          )
+                        : 0;
+                // calculate the ratio for the user (Math.round((kebabs.length / days) * 100) / 100)
+                const ratio = Math.round((kebabCount / days) * 100) / 100;
+                // store the ratio for the user in the ratios object
+                ratios[name] = ratio;
+            }
+            // sort the users by their ratio in descending order
+            const sortedUsers = Object.keys(ratios).sort(
+                (a, b) => ratios[b] - ratios[a]
+            );
+            // get the user's place in the leaderboard
+            if (customuser) {
+                placeinleaderboard =
+                    sortedUsers.findIndex((user) => user == user) + 1;
+            } else {
+                placeinleaderboard =
+                    sortedUsers.findIndex(
+                        (user) => user == interaction.user.id
+                    ) + 1;
+            }
+            switch (placeinleaderboard) {
+                case 1:
+                    reply += `:trophy: Meilleur ratio de la saison ${season}-${
+                        season + 1
+                    } (${ratios[sortedUsers[0]]} kebab/jour)\n`;
+                    break;
+                case 2:
+                    reply += `:second_place: Deuxième meilleur ratio de la saison ${season}-${
+                        season + 1
+                    } (${ratios[sortedUsers[1]]} kebab/jour)\n`;
+                    break;
+                case 3:
+                    reply += `:third_place: Troisième meilleur ratio de la saison ${season}-${
+                        season + 1
+                    } (${ratios[sortedUsers[2]]} kebab/jour)\n`;
+                    break;
+                default:
+                    break;
+            }
+        }
+        // get the user's place in the global leaderboard (all seasons)
+        allkebabs = await Kebab.aggregate([
+            { $group: { _id: "$user", count: { $sum: 1 } } },
+            { $sort: { count: -1 } },
+        ]);
+        if (customuser) {
+            placeinleaderboard =
+                allkebabs.findIndex((kebab) => kebab._id == user) + 1;
+        } else {
+            placeinleaderboard =
+                allkebabs.findIndex(
+                    (kebab) => kebab._id == interaction.user.id
+                ) + 1;
+        }
+        switch (placeinleaderboard) {
+            case 1:
+                reply += `:trophy: Plus gros mangeur de tous les temps (x${allkebabs[0].count})\n`;
+                break;
+            case 2:
+                reply += `:second_place: Deuxième plus gros mangeur de tous les temps (x${allkebabs[1].count})\n`;
+                break;
+            case 3:
+                reply += `:third_place: Troisième plus gros mangeur de tous les temps (x${allkebabs[2].count})\n`;
+                break;
+            default:
+                break;
+        }
+        await interaction.reply(reply);
+    },
+};
--- commands/profil.js
+++ commands/profil.js
@@ -7,6 +7,12 @@ module.exports = {
         .setDescription("Affiche un profil de gros mangeur de kebabs.")
         .addStringOption((option) =>
             option
+                .setName("saison")
+                .setDescription("La saison comptabilisée dans le profil.")
+                .setRequired(false)
+        )
+        .addStringOption((option) =>
+            option
                 .setName("user")
                 .setDescription(
                     "L'utilisateur dont vous voulez voir le profil."
@@ -19,6 +25,18 @@ module.exports = {
             user,
             reply = "",
             customuser = false;
+        // Get current year
+        const now = new Date();
+        const currentYear = now.getFullYear();
+        // If no specified season or it is not valid, use the current season (year)
+        let season =
+            parseInt(interaction.options.getString("saison")) || currentYear;
+        if (!season || season < 2022 || season > currentYear) {
+            season = currentYear;
+        }
+        // Get user profile for the kebabs between 1st september of season and 31st august of season+1
+        const start = new Date(season, 8, 1); // September 1st of variable season
+        const end = new Date(season + 1, 7, 31); // August 31st of season+1
         if (interaction.options.getString("user"))
             user = interaction.options.getString("user").replace(/[<@!>]/g, "");
         if (
@@ -26,14 +44,32 @@ module.exports = {
             user != interaction.user.id
         ) {
             customuser = true;
-            kebabs = await Kebab.find({ user: user }).sort({ datetime: 1 });
-            reply += `Profil de ${interaction.options.getString("user")} :\n`;
+            kebabs = await Kebab.find({
+                user: user,
+                datetime: {
+                    $gte: start,
+                    $lte: end,
+                },
+            }).sort({ datetime: 1 });
+            reply += `**Profil de ${interaction.options.getString(
+                "user"
+            )} pour la saison ${season}-${season + 1} :**\n\n`;
         } else {
-            kebabs = await Kebab.find({ user: interaction.user.id }).sort({
+            kebabs = await Kebab.find({
+                user: interaction.user.id,
+                datetime: {
+                    $gte: start,
+                    $lte: end,
+                },
+            }).sort({
                 datetime: 1,
             });
+            reply += `**Votre profil pour la saison ${season}-${
+                season + 1
+            } :**\n\n`;
         }
         const allkebabs = await Kebab.aggregate([
+            { $match: { datetime: { $gte: start, $lte: end } } },
             { $group: { _id: "$user", count: { $sum: 1 } } },
             { $sort: { count: -1 } },
         ]);
@@ -50,16 +86,18 @@ module.exports = {
         switch (kebabs.length) {
             case 0:
                 if (customuser) {
-                    reply += "Cette personne n'a jamais mangé de kebab.";
+                    reply +=
+                        "Cette personne n'a pas mangé de kebab cette saison.";
                 } else {
-                    reply += "Vous n'avez jamais mangé de kebab.";
+                    reply += "Vous n'avez pas mangé de kebab cette saison.";
                 }
                 break;
             case 1:
                 if (customuser) {
-                    reply += "Cette personne a mangé un seul kebab.";
+                    reply +=
+                        "Cette personne a mangé un seul kebab cette saison.";
                 } else {
-                    reply += `Vous avez mangé un seul kebab.
+                    reply += `Vous avez mangé un seul kebab cette saison.
 Il a été mangé le ${kebabs[0].datetime.toLocaleDateString("fr-FR", {
                         weekday: "long",
                         year: "numeric",
@@ -72,7 +110,7 @@ Il a été mangé le ${kebabs[0].datetime.toLocaleDateString("fr-FR", {
                 if (customuser) {
                     reply += `Cette personne a mangé ${kebabs.length} kebab${
                         kebabs.length > 1 ? "s" : ""
-                    }.
+                    } cette saison.
 Son premier kebab a été mangé le ${kebabs[0].datetime.toLocaleDateString(
                         "fr-FR",
                         {
@@ -93,7 +131,7 @@ Son dernier kebab a été mangé le ${kebabs[
                 } else {
                     reply += `Vous avez mangé ${kebabs.length} kebab${
                         kebabs.length > 1 ? "s" : ""
-                    }.
+                    } cette saison.
 Votre premier kebab a été mangé le ${kebabs[0].datetime.toLocaleDateString(
                         "fr-FR",
                         {
@@ -115,24 +153,24 @@ Votre dernier kebab a été mangé le ${kebabs[
                 break;
         }
         if (customuser) {
-            if (placeinleaderboard == 0) {
+            if (placeinleaderboard == 0 || kebabs.length == 0) {
                 reply +=
-                    "\nCette personne n'est pas dans le classement des mangeurs de kebabs.";
+                    "\nCette personne n'est pas dans le classement des mangeurs de kebabs de la saison.";
             } else if (placeinleaderboard == 1) {
                 reply +=
-                    "\nCette personne est première du classement des plus gros mangeurs de kebabs !";
+                    "\nCette personne est première du classement des plus gros mangeurs de kebabs de la saison !";
             } else {
-                reply += `\nCette personne est ${placeinleaderboard}ème du classement des plus gros mangeurs de kebabs.`;
+                reply += `\nCette personne est ${placeinleaderboard}ème du classement des plus gros mangeurs de kebabs de la saison.`;
             }
         } else {
-            if (placeinleaderboard == 0) {
+            if (placeinleaderboard == 0 || kebabs.length == 0) {
                 reply +=
-                    "\nVous n'êtes pas dans le classement des mangeurs de kebabs.";
+                    "\nVous n'êtes pas dans le classement des mangeurs de kebabs de la saison.";
             } else if (placeinleaderboard == 1) {
                 reply +=
-                    "\nVous êtes premier du classement des plus gros mangeurs de kebabs !";
+                    "\nVous êtes premier du classement des plus gros mangeurs de kebabs de la saison !";
             } else {
-                reply += `\nVous êtes ${placeinleaderboard}ème du classement des plus gros mangeurs de kebabs.`;
+                reply += `\nVous êtes ${placeinleaderboard}ème du classement des plus gros mangeurs de kebabs de la saison.`;
             }
         }
         await interaction.reply(reply);
