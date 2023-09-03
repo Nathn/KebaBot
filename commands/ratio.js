80cca9968efce831c1de16565d353b2e2d8417c2
Nathn <nathan@tranchant.fr>
2023-09-03 13:47:59 +0200

    Update ratio command to match seasons standards

--- commands/ratio.js
+++ commands/ratio.js
@@ -10,6 +10,14 @@ module.exports = {
                 .setName("user")
                 .setDescription("L'utilisateur dont vous voulez voir le ratio.")
                 .setRequired(false)
+        )
+        .addStringOption((option) =>
+            option
+                .setName("saison")
+                .setDescription(
+                    "La saison dont vous voulez voir le classement."
+                )
+                .setRequired(false)
         ),
     async execute(interaction) {
         const Kebab = mongoose.model("Kebab");
@@ -17,6 +25,17 @@ module.exports = {
             user,
             reply = "",
             customuser = false;
+        const now = new Date();
+        const currentYear = now.getFullYear();
+        // If no specified season or it is not valid, use the current season (year)
+        let season =
+            parseInt(interaction.options.getString("saison")) || currentYear;
+        if (!season || season < 2022 || season > currentYear) {
+            season = currentYear;
+        }
+        // Get users kebabs between 1st september of season and 31st august of season+1
+        const start = new Date(season, 8, 1); // September 1st of variable season
+        const end = new Date(season + 1, 7, 31); // August 31st of season+1
         if (interaction.options.getString("user"))
             user = interaction.options.getString("user").replace(/[<@!>]/g, "");
         if (
@@ -24,23 +43,42 @@ module.exports = {
             user != interaction.user.id
         ) {
             customuser = true;
-            kebabs = await Kebab.find({ user: user }).sort({ datetime: 1 });
-            reply += `Ratio de ${interaction.options.getString("user")} :\n`;
+            kebabs = await Kebab.find({
+                user: user,
+                datetime: {
+                    $gte: start,
+                    $lte: end,
+                },
+            }).sort({ datetime: 1 });
+            reply += `**Ratio de ${interaction.options.getString(
+                "user"
+            )} pour la saison ${season}-${season + 1} :**\n\n`;
         } else {
-            kebabs = await Kebab.find({ user: interaction.user.id }).sort({
+            kebabs = await Kebab.find({
+                user: interaction.user.id,
+                datetime: {
+                    $gte: start,
+                    $lte: end,
+                },
+            }).sort({
                 datetime: 1,
             });
+            reply += `**Votre ratio pour la saison ${season}-${
+                season + 1
+            } :**\n\n`;
         }
         let firstkebab,
-            now,
+            lastkebab,
             days = 0,
             dayswithoutweekends = 0;
         if (kebabs.length > 0) {
             firstkebab = kebabs[0].datetime;
-            now = new Date();
-            if (firstkebab && now) {
+            lastkebab = kebabs[kebabs.length - 1].datetime;
+            if (firstkebab && lastkebab) {
                 let firstkebabcopy = new Date(firstkebab);
-                days = Math.ceil((now - firstkebab) / (1000 * 60 * 60 * 24));
+                days = Math.ceil(
+                    (lastkebab - firstkebab) / (1000 * 60 * 60 * 24)
+                );
                 for (let i = 0; i < days; i++) {
                     if (
                         firstkebabcopy.getDay() != 0 &&
@@ -55,15 +93,17 @@ module.exports = {
         switch (kebabs.length) {
             case 0:
                 if (customuser) {
-                    reply += "Cette personne n'a jamais mangé de kebab.\n";
+                    reply +=
+                        "Cette personne n'a jamais mangé de kebab cette saison.\n";
                 } else {
-                    reply += "Vous n'avez jamais mangé de kebab.\n";
+                    reply +=
+                        "Vous n'avez jamais mangé de kebab cette saison.\n";
                 }
                 reply += "Ratio : 0 kebab par jour.";
                 break;
             case 1:
                 if (customuser) {
-                    reply += `Cette personne a mangé un seul kebab, depuis le ${firstkebab.toLocaleDateString(
+                    reply += `Cette personne a mangé un seul kebab cette saison, le ${firstkebab.toLocaleDateString(
                         "fr-FR",
                         {
                             weekday: "long",
@@ -73,7 +113,7 @@ module.exports = {
                         }
                     )}.\n`;
                 } else {
-                    reply += `Vous avez mangé un seul kebab, depuis le ${firstkebab.toLocaleDateString(
+                    reply += `Vous avez mangé un seul kebab cette saison, le ${firstkebab.toLocaleDateString(
                         "fr-FR",
                         {
                             weekday: "long",
@@ -113,7 +153,7 @@ module.exports = {
                             month: "long",
                             day: "numeric",
                         }
-                    )}.\n`;
+                    )} cette saison.\n`;
                 } else {
                     reply += `Vous avez mangé ${
                         kebabs.length
@@ -125,7 +165,7 @@ module.exports = {
                             month: "long",
                             day: "numeric",
                         }
-                    )}.\n`;
+                    )} cette saison.\n`;
                 }
                 reply += `Ratio : ${
                     Math.round((kebabs.length / days) * 100) / 100
